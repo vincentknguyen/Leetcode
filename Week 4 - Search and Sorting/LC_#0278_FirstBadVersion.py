86e3c5a74f5b713ed0bc5f50efc644462c991d43
vincentknguyen <82846960+vincentknguyen@users.noreply.github.com>
2021-08-20 15:32:23 -0400

    Update LC_#0278_FirstBadVersion.py

--- Week 4 - Search and Sorting/LC_#0278_FirstBadVersion.py
+++ Week 4 - Search and Sorting/LC_#0278_FirstBadVersion.py
@@ -6,23 +6,38 @@ class Solution(object):
     def firstBadVersion(self, n):
         #All the versions after a bad version are also bad
         
-        #Start by initializing the two pointers
-        left = 1
-        right = n
+        l,r = 1,n
+        
+        while(l < r):
+            mid = (l + r) //2
+            if isBadVersion(mid):
+                r = mid
+            else:
+                l = mid + 1
+                
+        return l
         
         #n versions [1, 2, ..., n]
         #0 0 0 1 1
         
-        #Time limit exceeded
+        
+        
+        #Vincent version original
+        
+        """
+        #Start by initializing the two pointers
+        left = 1
+        right = n
+        
         while left <= right:
             mid = (left + right)//2
             
-            """
-            First bad version is the n where:
-                isBadVersion(n-1) = False
-                isBadVersion(n) = True
-                isBadVersion(n+1) = True
-            """
+            
+                #First bad version is the n where:
+                #isBadVersion(n-1) = False
+                #isBadVersion(n) = True
+                #isBadVersion(n+1) = True
+            
         
             if isBadVersion(mid) == True:
                 
@@ -34,3 +49,6 @@ class Solution(object):
                 #Need to add +1 to the left index
                 #because we're trying to check what is the very first BAD version, not the last GOOD Version
                 left = mid+1
+                
+        return 1
+        """
