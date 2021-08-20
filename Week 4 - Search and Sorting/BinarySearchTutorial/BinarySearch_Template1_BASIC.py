80405697d0dde0400bae17dfba09e74b287aa948
vincentknguyen <82846960+vincentknguyen@users.noreply.github.com>
2021-08-20 16:17:27 -0400

    Update BinarySearch_Template1_BASIC.py

--- Week 4 - Search and Sorting/BinarySearchTutorial/BinarySearch_Template1_BASIC.py
+++ Week 4 - Search and Sorting/BinarySearchTutorial/BinarySearch_Template1_BASIC.py
@@ -3,7 +3,7 @@
 Used to search for an element or condition which can be determined by accessing a SINGLE index in the array
 
 No post-processing required because at each step, you are checking to see if the element has been found.
-If you reach the end, then you know the element is not found
+If you reach the end, then you know the element is NOT found
 
 
 Key Conditions:
