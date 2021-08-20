6a0a50f5424c2ab9a15758e94b219f08d7551d8d
vincentknguyen <82846960+vincentknguyen@users.noreply.github.com>
2021-08-20 14:51:15 -0400

    Update BinarySearch_Examples.py

--- Week 4 - Search and Sorting/BinarySearchTutorial/BinarySearch_Examples.py
+++ Week 4 - Search and Sorting/BinarySearchTutorial/BinarySearch_Examples.py
@@ -1,9 +1,18 @@
 """
-#1. Binary Seach Template 1
+#1. Binary Search Template 1
 Used to search for an element or condition which can be determined by accessing a SINGLE index in the array
 
 No post-processing required because at each step, you are checking to see if the element has been found.
 If you reach the end, then you know the element is not found
+
+
+Key Conditions:
+
+Initial Condition: left = 0, right = length-1
+Termination: left > right
+Searching Left: right = mid-1
+Searching Right: left = mid+1
+
 """
 
 
