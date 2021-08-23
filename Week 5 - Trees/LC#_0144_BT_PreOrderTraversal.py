3621f76e4f27704981c0ec2cc6ebea46725649fc
vincentknguyen <82846960+vincentknguyen@users.noreply.github.com>
2021-08-23 19:33:49 -0400

    Update LC#_0144_BT_PreOrderTraversal.py

--- Week 5 - Trees/LC#_0144_BT_PreOrderTraversal.py
+++ Week 5 - Trees/LC#_0144_BT_PreOrderTraversal.py
@@ -8,6 +8,35 @@ https://leetcode.com/problems/binary-tree-preorder-traversal/
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+'''
+     1
+    / \
+   2    3
+  / \  / \
+ 4  5  6  7
+
+#Recursive solution -> THINK OF IT AS A CALL TRACE
+[1] + 2 + 3
+[2] + 4 + 5
+[3] + 6 + 7
+
+'''
+
+'''
+#Stack solution
+stack = [3,2]
+result = [1]
+
+
+stack = [3,5,4]
+result = [1,2]
+
+
+stack = [3,5]
+result = [1,2,4,5]
+'''
+
 class Solution(object):
     def preorderTraversal(self, root):
         #Preorder = root, left, right
@@ -15,7 +44,6 @@ class Solution(object):
         #Base case: check when we need to exit the function
         if root is None:
             return []
-        
         
         """
         #1. Recursive case: Root, left right
@@ -25,23 +53,30 @@ class Solution(object):
         return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
         """
         
-        
-        #2. Iterative solution: using a stack (LIFO)
+        #2. Iterative solution: using a stack (LIFO), instead of a call stack
         stack = [root]
         result = []
         
+        
+        #whilt stack is not empty
         while stack != []:
+             
+            #pop -> removes and returns last value from ist
             root = stack.pop()
             result.append(root.val)
+            
+            #if a right child exists, add it first to the stack
             if root.right is not None:
                 stack.append(root.right)
-                
+            
+            
+            #if a left child exists, add it second to the stack            
             if root.left is not None:
                 stack.append(root.left)
         
         return result
         
-        
+      
                 
         
         
