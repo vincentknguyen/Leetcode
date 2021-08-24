c4c4a103598323050b4e69e93ccb05aaf20c26fc
vincentknguyen <82846960+vincentknguyen@users.noreply.github.com>
2021-08-24 11:02:33 -0400

    Update LC#_0094_BT_InOrderTraversal.py

--- Week 5 - Trees/LC#_0094_BT_InOrderTraversal.py
+++ Week 5 - Trees/LC#_0094_BT_InOrderTraversal.py
@@ -8,33 +8,66 @@ https://leetcode.com/problems/binary-tree-inorder-traversal/
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+'''
+     1
+    / \
+   2    3
+  / \  / \
+ 4  5  6  7
+
+'''
+
 class Solution(object):
     def inorderTraversal(self, root):
         #In order traversal = left, root, right
         
         #1. Recursive result
         """
-        res = []
-        if root:
-            res = self.inorderTraversal(root.left) 
-            res.append(root.val)
-            res = res + self.inorderTraversal(root.right)
-        return res
-        """
-                
-        #2. Iterative result
+        if root is None:
+            return []
+        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right) 
         
+        
+        #res = []
+        #if root:
+        #    res = self.inorderTraversal(root.left) 
+        #    res.append(root.val)
+        #    res = res + self.inorderTraversal(root.right)
+        #return res
+        """
+
         stack = []
-        result = []            
+        result = []
+        
+        #Left root right
         
         while root is not None or stack != []:
+            #you want to go all the way left first
             while root is not None:
                 stack.append(root)
                 root = root.left
+                
+            #stack = [1,]
+            #result = [4,2,5]
+            
+            #You have now just traversed everything on the left
+            #pop the current value in stack and append to result []
             root = stack.pop()
             result.append(root.val)
+            
+            #then check on the right
             root = root.right
+        
+        
         return result
+        
+    
+    
+        
+        
+
+        
         
     
     
