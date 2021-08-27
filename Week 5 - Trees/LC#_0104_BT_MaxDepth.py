c2e107d3c6e48d2b2d5f3f6cbf252ff78e1e4eda
vincentknguyen <82846960+vincentknguyen@users.noreply.github.com>
2021-08-27 14:41:25 -0400

    Update LC#_0104_BT_MaxDepth.py

--- Week 5 - Trees/LC#_0104_BT_MaxDepth.py
+++ Week 5 - Trees/LC#_0104_BT_MaxDepth.py
@@ -3,35 +3,65 @@ https://leetcode.com/problems/maximum-depth-of-binary-tree/
 '''
 
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def maxDepth(self, root):
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        #max depth of an empty tree is 0
         if not root:
             return 0
         
-        #1. Recursive Solution
-        
+        #1a. Recursive Solution (DFS)
+        #recursion is looking at the subproblem
         #return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
                 
-        #2. Iterative BFS, basically level
-        '''
-        level = 0
-        q = deque([root])
-        while q:
-            for i in range(len(q)):
-                node = q.popleft()
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
             
-            level += 1
-        return level
+        #1b. Recursive DFS
         '''
+        l = self.maxDepth(root.left)
+        r = self.maxDepth(root.right)
+        
+        if l > r:
+            return l + 1
+        return r + 1
+        '''
+                    
+            
+            
+        #2. Iterative BFS, basically level order traversal until you get to the last level
+        #then count back
+
+        
+        #Deques are a generalization of stacks and queues (the name is pronounced “deck” and is short for “double-ended queue”). 
+        #Deques support  thread-safe, memory efficient appends and pops from either side of the deque 
+        #with approximately the same O(1) performance in either direction.
+        
+        '''
+        #initialize
+        level = 0                
+        q = []
+        q.append(root)
+        
+        
+        #at each level you are trying to replace with the next
+                
+        while q:
+            level += 1
+            temp = []
+            
+            for node in q:
+                if node.left:
+                    temp.append(node.left)
+                if node.right:
+                    temp.append(node.right)
+            
+            q = temp
+        return depth
+        '''    
+        
         #3. Iterative DFS
         
         stack = [[root,1]]
@@ -44,4 +74,6 @@ class Solution(object):
                 stack.append([node.left, depth + 1])
                 stack.append([node.right, depth + 1])
                 
-        return res         
+        return res     
+        
+  
