4fa7f58c899cb4462587acc5b751e9d41e16fdbf
vincentknguyen <82846960+vincentknguyen@users.noreply.github.com>
2021-08-26 21:03:13 -0400

    Update CTCI_Ch1_ArraysAndStrings.py

--- Week 1 - Arrays/CTCI_Ch1_ArraysAndStrings.py
+++ Week 1 - Arrays/CTCI_Ch1_ArraysAndStrings.py
@@ -1,3 +1,7 @@
 #'hello world'
+'''
+Hash table = data structure that maps keys to values for efficient lookup
+Arraylist = array that resizes itself as needed, while still providing O(1) access
+  make sure you are comfortable with dynamically resizable arrays for given language
 
-
+'''
